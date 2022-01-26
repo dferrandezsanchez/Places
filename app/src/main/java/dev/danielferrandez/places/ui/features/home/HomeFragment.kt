50ad5760f438ab0f9c4a0b23e3653a315e95c456
Daniel Ferrández <d.ferrandez.sanchez@gmail.com>
2022-01-26 08:33:49 +0100

    add navigation graph, viewbinding, and fragmentBinding

---
+++ app/src/main/java/dev/danielferrandez/places/ui/features/home/HomeFragment.kt
@@ -0,0 +1,8 @@
+package dev.danielferrandez.places.ui.features.home
+
+import androidx.fragment.app.Fragment
+import dev.danielferrandez.places.R
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+}
---
+++ app/src/main/java/dev/danielferrandez/places/ui/features/home/HomeViewModel.kt
@@ -0,0 +1,9 @@
+package dev.danielferrandez.places.ui.features.home
+
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel(){
+
+
+
+}
